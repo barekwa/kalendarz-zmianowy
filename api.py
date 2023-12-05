42f1392f5be496d53ef9d41d795c0204f79e7be4
AdrianKwec <adrian.kwec11@gmail.com>
2023-12-05 03:00:16 +0100

    swagger and hashed passwords achieved

--- api.py
+++ api.py
@@ -4,12 +4,19 @@ from flask import Flask
 from flask import request, jsonify
 from flask_cors import CORS
 from pymongo import MongoClient
+import hashlib
+import secrets
 
 from auth.auth import generate_token, auth_required
 from schemas.CalendarSchema import CalendarResponse, CalendarRequest, EntryType
 from schemas.UserSchema import UserCreateRequest, UserLoginRequest
 
 SWAGGER_TEMPLATE = {
+    "swagger": "2.0",
+    "info": {
+        "version": "1.0",
+        "title": "Calendar API",
+    },
     "securityDefinitions": {
         "BearerAuth": {
             "type": "apiKey",
@@ -17,8 +24,132 @@ SWAGGER_TEMPLATE = {
             "in": "header",
             "description": "Enter your Bearer token in the format `Bearer <token>`",
         }
-    }
+    },
+    "definitions": {
+        "LoginResponse": {
+            "type": "object",
+            "properties": {
+                "token": {"type": "string", "example": "example_token"}
+            },
+            "required": ["token"]
+        },
+        "UserLoginRequest": {
+            "type": "object",
+            "properties": {
+                "username": {"type": "string", "example": "example_username"},
+                "password": {"type": "string", "example": "example_password"}
+            },
+            "required": ["username", "password"]
+        },
+        "UserCreateRequest": {
+            "type": "object",
+            "properties": {
+                "username": {"type": "string", "example": "example_username"},
+                "mail": {"type": "string", "example": "example@mail.com"},
+                "password": {"type": "string", "example": "example_password"},
+            },
+        },
+        "CalendarResponse": {
+            "type": "object",
+            "properties": {
+                "_id": {"type": "string", "example": "example_id"},
+                "date": {"type": "string", "example": "2023-12-01"},
+                "entry_type": {"type": "string", "example": "work"},
+                "work_hours": {"type": "number", "example": 8},
+            },
+        },
+        "CalendarRequest": {
+            "type": "object",
+            "properties": {
+                "date": {"type": "string", "example": "2023-12-01"},
+                "entry_type": {"type": "string", "enum": ["work", "business_trip", "vacation", "sick_leave"], "example": "work"},
+                "work_hours": {"type": "number", "example": 8},
+                "user_id": {"type": "string", "example": "example_user_id"},
+            },
+        },
+        "IdRequest": {
+            "type": "object",
+            "properties": {
+                "entry_id": {"type": "string", "example": "example_entry_id"}
+            },
+            "required": ["entry_id"]
+        },
+
+    },
+    "paths": {
+        "/api/calendar/getById/{entry_id}": {
+            "get": {
+                "summary": "Get a calendar entry by ID for the authenticated user",
+                "tags": ["Calendar"],
+                "security": [{"BearerAuth": []}],
+                "parameters": [
+                    {
+                        "name": "entry_id",
+                        "in": "path",
+                        "type": "string",
+                        "required": True,
+                        "description": "The ID of the entry to be retrieved.",
+                    }
+                ],
+                "responses": {
+                    "200": {
+                        "description": "Details of the calendar entry.",
+                        "schema": {"$ref": "#/definitions/CalendarResponse"},
+                    },
+                    "404": {"description": "Entry not found."},
+                },
+            },
+        },
+        "/api/calendar/update/{entry_id}": {
+            "put": {
+                "summary": "Update an existing calendar entry for the authenticated user",
+                "tags": ["Calendar"],
+                "security": [{"BearerAuth": []}],
+                "parameters": [
+                {
+                    "name": "entry_id",
+                    "in": "path",
+                    "type": "string",
+                    "required": True,
+                    "description": "The ID of the entry to be updated.",
+                    "example": "example_id"
+                },
+                {
+                    "name": "date",
+                    "in": "body",
+                    "type": "string",
+                    "required": True,
+                    "description": "The updated date of the entry.",
+                    "example": "2023-12-01",
+                },
+                {
+                    "name": "entry_type",
+                    "in": "body",
+                    "type": "string",
+                    "enum": ["work", "business_trip", "vacation", "sick_leave"],
+                    "required": True,
+                    "description": "The updated type of the entry.",
+                    "example": "work",
+                },
+                {
+                    "name": "work_hours",
+                    "in": "body",
+                    "type": "number",
+                    "description": "The updated number of work hours for the entry (optional).",
+                    "example": 8,
+                },
+            ],
+            "responses": {
+                "204": {"description": "Entry updated successfully."},
+                "400": {"description": "Bad Request. Missing or invalid data."},
+                "404": {"description": "Entry not found."},
+            },
+            },
+        },
+    },
 }
+
+
 app = Flask(__name__)
 CORS(app)
 swagger = Swagger(app, template=SWAGGER_TEMPLATE)
@@ -40,25 +171,16 @@ def user_login():
         tags:
           - Authorization
         parameters:
-          - name: username
+          - name: body
             in: body
-            type: string
             required: true
-            description: The username of the user.
-          - name: password
-            in: body
-            type: string
-            required: true
-            description: The password of the user.
+            schema:
+              $ref: "#/definitions/UserLoginRequest"
         responses:
           200:
             description: Successful login. Returns a JWT token.
             schema:
-              type: object
-              properties:
-                token:
-                  type: string
-                  description: JWT token for authentication.
+              $ref: '#/definitions/LoginResponse'
             examples:
               {'token': 'your_generated_token_here'}
           401:
@@ -87,21 +209,12 @@ def user_register():
         tags:
           - Authorization
         parameters:
-          - name: username
+          - name: body
             in: body
-            type: string
             required: true
-            description: The username of the new user.
-          - name: mail
-            in: body
-            type: string
-            required: true
-            description: The email address of the new user.
-          - name: password
-            in: body
-            type: string
-            required: true
-            description: The password of the new user.
+            schema:
+              $ref: "#/definitions/UserCreateRequest"
+
         responses:
           201:
             description: Registered successfully.
@@ -155,6 +268,8 @@ def get_all_entries(user_id):
               - _id
               - date
               - entry_type
+            schema:
+                $ref: "#/definitions/CalendarResponse"
       401:
         description: Unauthorized.
     """
@@ -180,22 +295,13 @@ def add_entry(user_id):
     security:
       - BearerAuth: []
     parameters:
-      - name: date
+      - name: body
         in: body
         type: string
         required: true
-        description: The date of the new entry.
-      - name: entry_type
-        in: body
-        type: string
-        enum: ['work', 'business_trip', 'vacation', 'sick_leave']
-        required: true
-        description: >
-          The updated type of the entry. Should be one of: 'business_trip', 'business_trip', 'vacation','sick_leave'.
-      - name: work_hours
-        in: body
-        type: number
-        description: The number of work hours for the new entry (optional).
+        schema:
+            $ref: "#/definitions/CalendarRequest"
+
     responses:
       201:
         description: New entry added successfully.
@@ -206,6 +312,8 @@ def add_entry(user_id):
               type: string
         examples:
           {'entry_id': 'your_generated_entry_id_here'}
+        schema:
+            $ref: "#/definitions/CalendarResponse"
       400:
         description: Bad Request. Missing or invalid data.
     """
@@ -243,6 +351,8 @@ def delete_entry(user_id, entry_id):
         type: string
         required: true
         description: The ID of the entry to be deleted.
+        schema:
+              $ref: "#/definitions/IdRequest"
     responses:
       204:
         description: Entry deleted successfully.
@@ -273,24 +383,13 @@ def get_entry(user_id, entry_id):
         type: string
         required: true
         description: The ID of the entry to be retrieved.
+        schema:
+              $ref: "#/definitions/IdRequest"
     responses:
       200:
         description: Details of the calendar entry.
         schema:
-          type: object
-          properties:
-            _id:
-              type: string
-            date:
-              type: string
-            entry_type:
-              type: string
-            work_hours:
-              type: number
-          required:
-            - _id
-            - date
-            - entry_type
+            $ref: "#/definitions/CalendarRequest"
       404:
         description: Entry not found.
     """
@@ -323,22 +422,16 @@ def edit_entry(user_id, entry_id):
            type: string
            required: true
            description: The ID of the entry to be updated.
-         - name: date
+           schema:
+              $ref: "#/definitions/IdRequest"
+         - name: body
            in: body
            type: string
            required: true
-           description: The updated date of the entry.
-         - name: entry_type
-           in: body
-           type: string
-           enum: ['work', 'business_trip', 'vacation', 'sick_leave']
-           required: true
-           description: >
-             The updated type of the entry. Should be one of: 'business_trip', 'business_trip', 'vacation','sick_leave'.
-         - name: work_hours
-           in: body
-           type: number
-           description: The updated number of work hours for the entry (optional).
+           schema:
+            $ref: "#/definitions/CalendarRequest"
+
+
        responses:
          204:
            description: Entry updated successfully.
@@ -379,10 +472,23 @@ def validate_entry(calendar_entry):
         return False
     return True
 
+def hash_password(password, salt):
+    hashed_password = hashlib.sha256((password + salt).encode('utf-8')).hexdigest()
+    return hashed_password
 
 def register(user):
     if not user_exists(user.username):
-        user_collection.insert_one(user.to_dict())
+        # Generujemy solę
+        salt = secrets.token_hex(16)
+        # Haszujemy hasło z uwzględnieniem soli
+        hashed_password = hash_password(user.password, salt)
+
+        user_collection.insert_one({
+            'username': user.username,
+            'mail': user.mail,
+            'hashed_password': hashed_password,
+            'salt': salt
+        })
         return True
     return False
 
@@ -392,9 +498,11 @@ def user_exists(username):
 
 
 def login(user):
-    user = user_collection.find_one(user.to_dict())
-    if user:
-        return True
+    stored_user = user_collection.find_one({'username': user.username})
+    if stored_user:
+        hashed_password = hash_password(user.password, stored_user['salt'])
+        if hashed_password == stored_user['hashed_password']:
+            return True
     return False
 
 
